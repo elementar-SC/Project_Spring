30fb89781612b6aba9ea8467fa3ef2cdf30407a3
elementar-SC <107786191+elementar-SC@users.noreply.github.com>
2022-08-05 09:35:06 +0900

    Initial commit

---
+++ project/src/main/java/com/hanul/lee/LeeMemberVO.java
@@ -0,0 +1,87 @@
+package com.hanul.lee;
+
+public class LeeMemberVO {
+	private String id, pw ,phone, address, post, name, admin, salt, salt_pw, social;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPost() {
+		return post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(String admin) {
+		this.admin = admin;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public String getSalt_pw() {
+		return salt_pw;
+	}
+
+	public void setSalt_pw(String salt_pw) {
+		this.salt_pw = salt_pw;
+	}
+
+	public String getSocial() {
+		return social;
+	}
+
+	public void setSocial(String social) {
+		this.social = social;
+	}
+	
+	
+}
